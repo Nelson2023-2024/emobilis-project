13e4e8e024d1d3277801ab66bb6f7650b4edaedb
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-23 16:24:44 +0300

    Setup model for Users

---
+++ authentication/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ]
+    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=100)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    password = models.CharField(max_length=255)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    user_level = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
