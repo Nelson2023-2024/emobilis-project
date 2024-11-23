a138cc621ab6b891a9e71c9e83fa9a91642440da
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-23 19:06:42 +0300

    Added the login page

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,redirect
 from authentication.models import User
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
+        
+         # Hash the password before saving the user
+        hashed_password = make_password(request.POST['pass1'])
         
         user = User(
             first_name = request.POST['fname'],
@@ -12,7 +16,7 @@ def register(request):
             email = request.POST['email'],
             phone_number = request.POST['phoneNo'],
             gender = request.POST['gender'],
-            password = request.POST['pass1'],
+            password = hashed_password
         )
         
         user.save()
@@ -20,3 +24,11 @@ def register(request):
         
     else:
         return render(request, 'register.html')
+    
+    
+def login(request):
+    if request.method == "POST":
+        pass
+        
+    else:
+        return render(request, 'login.html')
