1e3ad53a5c05137c94b8ebbcd19891e2b141ca18
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-29 19:05:04 +0300

    Upload image feature working

--- authentication/models.py
+++ authentication/models.py
@@ -23,7 +23,7 @@ class User(models.Model):
     
 class Category(models.Model):
     product_title = models.CharField(max_length=100)
-    product_image = models.CharField(max_length=255)
+    product_image = models.ImageField(default='fallback.png', blank=True)  # Specify the upload path
     product_price = models.CharField(max_length=100)
     product_description = models.TextField()
     registration_date = models.DateTimeField(auto_now_add=True)
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('register/', views.register, name="register"),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('pricing/', views.pricing, name="pricing"),
     path('logout/', views.logout, name='logout'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
