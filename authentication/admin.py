89853016d88243dad5c0476434ae964b08cc2180
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-30 11:10:14 +0300

    Created a contact model and registered it

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from authentication.models import User,Category
+from authentication.models import User,Category,Contact
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Category)
+admin.site.register(Contact)
--- authentication/models.py
+++ authentication/models.py
@@ -30,3 +30,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.product_title
+    
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    query = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.email}'
